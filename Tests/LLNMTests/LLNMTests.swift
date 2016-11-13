1ef1446ac1efad4a8918f6a4e6c2fb0b80bae453
Richard Wei <rxrwei@gmail.com>
2016-11-13 01:51:56 -0600

    Remove strides from shape

--- Tests/LLNMTests/LLNMTests.swift
+++ Tests/LLNMTests/LLNMTests.swift
@@ -7,14 +7,12 @@ class LLNMTests: XCTestCase {
         let tensor = TensorDescriptor<Float>(shape: [1, 2, 3, 4, 5, 6, 7, 8])
         XCTAssertEqual(tensor.shape.rank, 8)
         XCTAssertEqual(tensor.shape.dimensions, [1, 2, 3, 4, 5, 6, 7, 8])
-        XCTAssertEqual(tensor.shape.strides, [1, 1, 1, 1, 1, 1, 1, 1])
     }
     
     func testTensor() {
         let tensor = Tensor<Float>(shape: [1, 2, 3, 4, 5, 6, 7, 8], repeating: 0.0)
         XCTAssertEqual(tensor.shape.rank, 8)
         XCTAssertEqual(tensor.shape.dimensions, [1, 2, 3, 4, 5, 6, 7, 8])
-        XCTAssertEqual(tensor.shape.strides, [1, 1, 1, 1, 1, 1, 1, 1])
         XCTAssertEqual(tensor[0, 1, 2, 3, 4, 5, 6, 7].value, 0)
     }
 
