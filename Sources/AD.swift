fd6f079ac37185cccf3fb5c7bfe401f3a3a972e0
Richard Wei <rxrwei@gmail.com>
2016-11-20 19:10:14 -0600

    BLAS operators

--- Sources/AD.swift
+++ Sources/AD.swift
@@ -31,21 +31,8 @@ extension Assignment {
                 }
             }
         case let .mul(lhs, rhs):
-            var one: DataType = 1
-            var zero = DataType.zero
-            self.data.withUnsafeMutableDeviceAddress { ptrC -> () in
-                lhs.data.withUnsafeDeviceAddress { ptrA in
-                    rhs.data.withUnsafeDeviceAddress { ptrB in
-                        !!cudnnOpTensor(
-                            graph.dnn.handle,
-                            graph.tensorOperators.mulOp,
-                            &one, lhs.data.descriptor.handle, ptrA,
-                            &one, rhs.data.descriptor.handle, ptrB,
-                            &zero, self.data.descriptor.handle, ptrC
-                        )
-                    }
-                }
-            }
+            // TODO 
+            break
 
         case let .min(lhs, rhs):
             var one: DataType = 1
@@ -137,6 +124,60 @@ extension Assignment {
                 }
             }
 
+        case let .negative(x):
+            var one: DataType = -1
+            var zero = DataType.zero
+            self.data.withUnsafeMutableDeviceAddress { dest -> () in
+                x.data.withUnsafeDeviceAddress { src in
+                    !!cudnnAddTensor(
+                        graph.dnn.handle,
+                        &one, x.data.descriptor.handle, src,
+                        &zero, data.descriptor.handle, dest
+                    )
+                }
+            }
+
+        case let .scalarComplement(lhs, rhs):
+            var minusOne: DataType = -1
+            var one: DataType = 1
+            var lhs = lhs
+            self.data.withUnsafeMutableDeviceAddress { dest -> () in
+                rhs.data.withUnsafeDeviceAddress { src in
+                    !!cudnnSetTensor(
+                        graph.dnn.handle,
+                        data.descriptor.handle, dest, &lhs)
+                    !!cudnnTransformTensor(
+                        graph.dnn.handle,
+                        &minusOne, rhs.data.descriptor.handle, src,
+                        &one, data.descriptor.handle, dest)
+                }
+            }
+
+        case let .product(lhs, rhs):
+            var one: DataType = 1
+            var zero = DataType.zero
+            self.data.withUnsafeMutableDeviceAddress { ptrC -> () in
+                lhs.data.withUnsafeDeviceAddress { ptrA in
+                    rhs.data.withUnsafeDeviceAddress { ptrB in
+                        !!cudnnOpTensor(
+                            graph.dnn.handle,
+                            graph.tensorOperators.mulOp,
+                            &one, lhs.data.descriptor.handle, ptrA,
+                            &one, rhs.data.descriptor.handle, ptrB,
+                            &zero, self.data.descriptor.handle, ptrC
+                        )
+                    }
+                }
+            }
+
+        case let .log(x):
+            break
+
+        case let .input(shape: shape):
+            break
+
+        case let .parameter(shape: shape, initial: initializer):
+            break
         }
 
     }
