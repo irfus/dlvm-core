a886a1f4ffee26dccb36adecae0b1113485cd8c2
Richard Wei <rxrwei@gmail.com>
2016-12-23 01:29:30 +0100

    Some tests

--- Sources/telc/main.swift
+++ Sources/telc/main.swift
@@ -1,2 +1,19 @@
+import TEL
+import Foundation
 
-
+do {
+    guard let file = CommandLine.arguments.dropFirst().first else {
+        print("No file given")
+        exit(0)
+    }
+//    guard let path = Bundle.main.path(forResource: file, ofType: nil) else {
+//        print("Cannot open file \(file)")
+//        exit(0)
+//    }
+    let telSource = try String(contentsOfFile: file, encoding: .utf8)
+    let ast = try ProgramTree.parser.parse(telSource)
+    dump(ast)
+}
+catch {
+    print(error)
+}
