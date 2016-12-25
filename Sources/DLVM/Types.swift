4d9833b9da8e448f480396004915eedf24700cf7
Richard Wei <rxrwei@gmail.com>
2016-12-25 10:44:05 +0100

    Add type predicates

--- Sources/DLVM/Types.swift
+++ Sources/DLVM/Types.swift
@@ -9,6 +9,19 @@
 public enum DataType {
     case int1, int8, int16, int32, int64
     case float8, float16, float32, float64
+
+    public var isInt: Bool {
+        switch self {
+        case .int1, .int8, .int16, .int32, .int64:
+            return true
+        default:
+            return false
+        }
+    }
+
+    public var isFloat: Bool {
+        return !isInt
+    }
 }
 
 public protocol DataProtocol {
