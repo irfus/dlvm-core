770d9c2c98691d627f7b0b4a4a991c958e93078b
Richard Wei <rxrwei@gmail.com>
2016-12-25 13:56:17 +0100

    Implement custom printing for AST

--- Sources/TEL/AST.swift
+++ Sources/TEL/AST.swift
@@ -70,3 +70,92 @@ public struct ProgramTree {
     public let statements: [Statement]
 }
 
+extension Constant : CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case let .int(i):   return i.description
+        case let .float(f): return f.description
+        }
+    }
+}
+
+extension Variable : CustomStringConvertible {
+
+    public var description: String {
+        switch self {
+        case let .simple(name): return name
+        case let .recurrent(name, timestep: t, offset: offset):
+            return name + "[" + t + offset.description + "]"
+        }
+    }
+    
+}
+
+extension Expression : CustomStringConvertible {
+
+    public var description: String {
+        switch self {
+        case let .constant(c): return c.description
+        case let .random(lhs, rhs): return "(random \(lhs) \(rhs))"
+        case let .variable(v): return v.name
+        case let .call(name, args):
+            return "(\(name) \(args.map{$0.description}.joined(separator: " ")))"
+        case let .negate(expr):
+            return "(- \(expr))"
+        case let .add(lhs, rhs):
+            return "(+ \(lhs) \(rhs))"
+        case let .sub(lhs, rhs):
+            return "(+ \(lhs) \(rhs))"
+        case let .mul(lhs, rhs):
+            return "(* \(lhs) \(rhs))"
+        case let .product(lhs, rhs):
+            return "(⊗ \(lhs) \(rhs))"
+        case let .concat(exprs):
+            return "(concat \(exprs.map{$0.description}.joined(separator: " ")))"
+        }
+    }
+    
+}
+
+extension Declaration : CustomStringConvertible {
+
+    public var description: String {
+        switch self {
+        case let .assignment(v, role, dim, expr):
+            return "(define \(v):\(role)[\(dim.map{$0.description}.joined(separator: "x"))] \(expr))"
+        case let .recurrence(timestep, decls):
+            return "(recurrent \(timestep) \(decls.map{$0.description}.joined(separator: " ")))"
+        }
+    }
+    
+}
+
+extension Macro : CustomStringConvertible {
+
+    public var description: String {
+        switch self {
+        case let .type(t): return "(#type \(t))"
+        }
+    }
+
+}
+
+extension Statement : CustomStringConvertible {
+
+    public var description: String {
+        switch self {
+        case let .macro(m): return m.description
+        case let .declaration(decl): return decl.description
+        }
+    }
+    
+}
+
+extension ProgramTree : CustomStringConvertible {
+
+    public var description: String {
+        return "(program \(statements.map{$0.description}.joined(separator: " ")))"
+    }
+    
+}
+
