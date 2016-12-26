12dd0e44263c10d47eaa7725bef23ef657073cfd
Richard Wei <rxrwei@gmail.com>
2016-12-26 11:19:04 +0100

    More work on core components

--- Sources/DLVM/IRBuilder.swift
+++ Sources/DLVM/IRBuilder.swift
@@ -8,10 +8,65 @@
 
 public class IRBuilder {
 
-    let module: Module
-    
+    public let module: Module
+    public internal(set) var currentBlock: BasicBlock?
+
+    fileprivate var globalNameId: Int = 0
+    fileprivate var nameIdTable: [String : Int] = [:]
+
     public init() {
         module = Module()
     }
+
+}
+
+public extension IRBuilder {
+
+    public func makeName() -> String {
+        return disambiguatedName(for: "v\(globalNameId)")
+    }
+
+    public func disambiguatedName(for name: String) -> String {
+        if let id = nameIdTable[name] {
+            nameIdTable[name] = id + 1
+            return name + ".\(id)"
+        }
+        nameIdTable[name] = 1
+        return name
+    }
     
+}
+
+public extension IRBuilder {
+
+    @discardableResult
+    public func beginBasicBlock(named name: String) -> BasicBlock {
+        let block = BasicBlock(name: name)
+        currentBlock = block
+        return block
+    }
+
+    @discardableResult
+    public func declareTensor(named name: String, dataType: DataType,
+                              shape: TensorShape) -> Tensor {
+        let tensor = Tensor(name: name, dataType: dataType, shape: shape)
+        module.declare(tensor)
+        return tensor
+    }
+
+    @discardableResult
+    public func declareScalar(named name: String,
+                              type: ScalarType) -> Scalar {
+        let scalar = Scalar(name: name, type: type)
+        module.declare(scalar)
+        return scalar
+    }
+
+    @discardableResult
+    public func build(_ instructionKind: Instruction.Kind,
+                      named name: String? = nil) -> Variable {
+        let instruction = Instruction(kind: instructionKind)
+        return instruction.makeVariable(named: name ?? makeName())
+    }
+
 }
--- Sources/DLVM/Instruction.swift
+++ Sources/DLVM/Instruction.swift
@@ -7,13 +7,16 @@
 //
 
 public class Instruction : IRObject {
+    public enum ComparisonOperator {
+        case lt, leq, gt, geq, eq
+    }
     public enum Kind {
         case negate(Operand)
         case add(Operand, Operand)
         case mul(Operand, Operand)
         case min(Operand, Operand)
         case max(Operand, Operand)
-        case compare(Operand, Operand)
+        case compare(ComparisonOperator, Operand, Operand)
         case dotProduct(Tensor, Tensor)
         case random(Scalar, Scalar)
         case product(Tensor, Tensor)
@@ -34,6 +37,28 @@ public class Instruction : IRObject {
     public init(kind: Kind) {
         self.kind = kind
     }
+}
+
+extension Instruction : VariableProducer {
+
+    public func makeVariable(named name: String) -> Variable {
+        switch kind {
+        case let .negate(op as Tensor),
+             let .add(op as Tensor, _), let .add(_, op as Tensor),
+             let .mul(op as Tensor, _), let .mul(_, op as Tensor),
+             let .min(op as Tensor, _), let .min(_, op as Tensor),
+             let .max(op as Tensor, _), let .max(_, op as Tensor),
+             let .dotProduct(op, _),
+             let .activation(_, op),
+             let .transfer(_, op):
+            return Tensor(name: name, dataType: op.dataType,
+                          shape: op.shape, definition: self)
+
+        default: /// TODO
+            return UnavailableVariable.shared
+        }
+    }
+
 }
 
 // MARK: - Hashable
