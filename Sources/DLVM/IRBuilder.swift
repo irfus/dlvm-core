9a12b493531ee3970e6ad6a386b1992f78849d47
Richard Wei <rxrwei@gmail.com>
2016-12-26 15:58:22 +0100

    Reduce instruction kinds and implement IRBuilder

--- Sources/DLVM/IRBuilder.swift
+++ Sources/DLVM/IRBuilder.swift
@@ -7,9 +7,8 @@
 //
 
 public class IRBuilder {
-
     public let module: Module
-    public internal(set) var currentBlock: BasicBlock?
+    public var currentBlock: BasicBlock?
 
     fileprivate var globalNameId: Int = 0
     fileprivate var nameIdTable: [String : Int] = [:]
@@ -17,16 +16,16 @@ public class IRBuilder {
     public init() {
         module = Module()
     }
-
 }
 
+// MARK: - Name generation and disambiguation
 public extension IRBuilder {
 
-    public func makeName() -> String {
+    func makeName() -> String {
         return disambiguatedName(for: "v\(globalNameId)")
     }
 
-    public func disambiguatedName(for name: String) -> String {
+    func disambiguatedName(for name: String) -> String {
         if let id = nameIdTable[name] {
             nameIdTable[name] = id + 1
             return name + ".\(id)"
@@ -37,36 +36,118 @@ public extension IRBuilder {
     
 }
 
+// MARK: - Main builder API
 public extension IRBuilder {
 
     @discardableResult
-    public func beginBasicBlock(named name: String) -> BasicBlock {
-        let block = BasicBlock(name: name)
-        currentBlock = block
-        return block
-    }
-
-    @discardableResult
     public func declareTensor(named name: String, dataType: DataType,
-                              shape: TensorShape) -> Tensor {
-        let tensor = Tensor(name: name, dataType: dataType, shape: shape)
+                              shape: TensorShape) -> TensorVariable {
+        let tensor = TensorVariable(name: name, dataType: dataType, shape: shape)
         module.declare(tensor)
         return tensor
     }
 
     @discardableResult
     public func declareScalar(named name: String,
-                              type: ScalarType) -> Scalar {
-        let scalar = Scalar(name: name, type: type)
+                              type: ScalarType) -> ScalarVariable {
+        let scalar = ScalarVariable(name: name, type: type)
         module.declare(scalar)
         return scalar
     }
 
     @discardableResult
-    public func build(_ instructionKind: Instruction.Kind,
-                      named name: String? = nil) -> Variable {
+    public func makeBasicBlock(named name: String) -> BasicBlock {
+        let block = BasicBlock(name: disambiguatedName(for: name))
+        currentBlock = block
+        module.append(block)
+        return block
+    }
+
+    @inline(__always)
+    @discardableResult
+    func build(_ instructionKind: Instruction.Kind, named name: String? = nil) -> Variable {
+        precondition(currentBlock != nil, "Current basic block unavailable")
         let instruction = Instruction(kind: instructionKind)
+        currentBlock!.append(instruction)
         return instruction.makeVariable(named: name ?? makeName())
+    }
+
+    /// Addition of the same type
+    @discardableResult
+    public func makeBinaryOperation<T: Variable>(
+        _ `operator`: Instruction.BinaryOperator,
+        _ lhs: T, _ rhs: T, name: String? = nil) -> T {
+        return build(.binaryOp(`operator`, lhs, rhs), named: name) as! T
+    }
+
+    /// Addition of any operand with tensor
+    @discardableResult
+    public func makeBinaryOperation(
+        _ `operator`: Instruction.BinaryOperator
+        , _ lhs: Operand, _ rhs: TensorVariable, name: String? = nil) -> TensorVariable {
+        return build(.binaryOp(`operator`, lhs, rhs), named: name) as! TensorVariable
+    }
+
+    /// Addition of any operand with tensor
+    @discardableResult
+    public func makeBinaryOperation(
+        _ `operator`: Instruction.BinaryOperator
+        , _ lhs: TensorVariable, _ rhs: Operand, name: String? = nil) -> TensorVariable {
+        return build(.binaryOp(`operator`, lhs, rhs), named: name) as! TensorVariable
+    }
+
+    @discardableResult
+    public func makeComparison(_ `operator`: Instruction.ComparisonOperator,
+                               _ lhs: Operand, _ rhs: Operand,
+                               name: String? = nil) -> ScalarVariable {
+        return build(.compare(`operator`, lhs, rhs), named: name) as! ScalarVariable // bool
+    }
+
+    @discardableResult
+    public func makeDotProduct(_ lhs: TensorVariable, _ rhs: TensorVariable,
+                               name: String? = nil) -> TensorVariable {
+        return build(.dotProduct(lhs, rhs), named: name) as! TensorVariable
+    }
+
+    @discardableResult
+    public func makeProduct(_ lhs: TensorVariable, _ rhs: TensorVariable,
+                            name: String? = nil) -> TensorVariable {
+        return build(.product(lhs, rhs), named: name) as! TensorVariable
+    }
+
+    @discardableResult
+    public func makeActivation(_ function: ActivationFunction, _ argument: TensorVariable,
+                               name: String? = nil) -> TensorVariable {
+        return build(.activation(function, argument), named: name) as! TensorVariable
+    }
+
+    @discardableResult
+    public func makeTransformation(
+        _ function: TransformationFunction, _ argument: TensorVariable,
+        name: String? = nil) -> TensorVariable {
+        return build(.transformation(function, argument), named: name) as! TensorVariable
+    }
+
+    @discardableResult
+    public func makeConcatenation(
+        _ arguments: [TensorVariable], name: String? = nil) -> TensorVariable {
+        return build(.concat(arguments), named: name) as! TensorVariable
+    }
+
+    @discardableResult
+    public func makePhi<T: Variable>(_ variables: T..., name: String? = nil) -> T {
+        return build(.phi(variables), named: name) as! T
+    }
+
+    @discardableResult
+    public func makeBranch(condition: Variable,
+                           thenBlock: BasicBlock, elseBlock: BasicBlock) {
+        build(.condBranch(condition, then: thenBlock, else: elseBlock))
+    }
+
+    @discardableResult
+    public func makeBranch(_ block: BasicBlock) {
+        build(.uncondBranch(block))
     }
 
 }
