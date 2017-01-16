92f312cf0077bcbeca26397d7bf2745f2cb8227f
Richard Wei <rxrwei@gmail.com>
2017-01-16 14:16:36 -0600

    Fix comment syntax

--- Sources/TEL/Parse.swift
+++ Sources/TEL/Parse.swift
@@ -13,7 +13,7 @@ import func Funky.flip
 /// Local primitive parsers
 fileprivate let identifier = Lexer.regex("[a-zA-Z_][a-zA-Z0-9_]*")
 fileprivate let number = Lexer.unsignedInteger ^^ { Int($0)! } .. "a number"
-fileprivate let lineComments = ("//" ~~> Lexer.string(until: "\n") <~~ Lexer.newLine)+
+fileprivate let lineComments = ("//" ~~> Lexer.string(until: "\n").maybeEmpty() <~~ Lexer.newLine)+
 fileprivate let spaces = (Lexer.whitespace | Lexer.tab)+
 fileprivate let newLines = Lexer.newLine+
 fileprivate let linebreaks = (newLines | lineComments).amid(spaces.?)+ .. "a linebreak"
