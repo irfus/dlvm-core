7d93a11a5fdef7a07cdbbcd77463b13f49e500cc
Richard Wei <rxrwei@gmail.com>
2017-01-15 19:57:11 -0600

    Implement most of the IR parser... on a flight

--- Sources/dlc/AST.swift
+++ Sources/dlc/AST.swift
@@ -7,21 +7,16 @@
 //
 
 import struct Parsey.SourceRange
-import enum DLVM.TypeBase
-import protocol DLVM.DataType
-import struct DLVM.ScalarType
-import struct DLVM.TensorType
-import enum DLVM.Immediate
 
 protocol ASTNode {
-    var sourceRange: SourceRange { get }
+    var range: SourceRange { get }
 }
 
 struct ModuleNode : ASTNode {
     let name: String
     let declarations: [DeclarationNode]
     let basicBlocks: [BasicBlockNode]
-    let sourceRange: SourceRange
+    let range: SourceRange
 }
 
 struct DeclarationNode : ASTNode {
@@ -29,38 +24,106 @@ struct DeclarationNode : ASTNode {
         case input, output, parameter
     }
     let name: String
-    let sourceRange: SourceRange
+    let range: SourceRange
 }
 
 struct BasicBlockNode : ASTNode {
     let name: String
     let isGradient: Bool
     let instructions: [InstructionNode]
-    let sourceRange: SourceRange
+    let range: SourceRange
+}
+
+struct ShapeNode : ASTNode {
+    let dimensions: [Int]
+    let range: SourceRange
+}
+
+struct InstructionDeclarationNode : ASTNode {
+    let name: String?
+    let instruction: InstructionNode
+    let range: SourceRange
 }
 
 enum InstructionNode : ASTNode {
+    case neg(OperandNode, SourceRange)
     case tmul(OperandNode, OperandNode, SourceRange)
-    
-    var sourceRange: SourceRange {
+    case add(OperandNode, OperandNode, SourceRange)
+    case sub(OperandNode, OperandNode, SourceRange)
+    case mul(OperandNode, OperandNode, SourceRange)
+    case div(OperandNode, OperandNode, SourceRange)
+    case min(OperandNode, OperandNode, SourceRange)
+    case max(OperandNode, OperandNode, SourceRange)
+    case sigmoid(OperandNode, SourceRange)
+    case tanh(OperandNode, SourceRange)
+    case relu(OperandNode, SourceRange)
+    case log(OperandNode, SourceRange)
+    case softmax(OperandNode, SourceRange)
+    case concat([OperandNode], Int?, SourceRange)
+    case shapeCast(OperandNode, ShapeNode, SourceRange)
+    case typeCast(OperandNode, TypeNode, SourceRange)
+    case store(OperandNode, OperandNode, SourceRange)
+
+    var range: SourceRange {
         switch self {
-        case let .tmul(_, _, sr):
+        case let .neg(_, sr),
+             let .tmul(_, _, sr),
+             let .add(_, _, sr), let .sub(_, _, sr),
+             let .mul(_, _, sr), let .div(_, _, sr),
+             let .min(_, _, sr), let .max(_, _, sr),
+             let .softmax(_, sr),
+             let .sigmoid(_, sr), let .relu(_, sr), let .tanh(_, sr), let .log(_, sr),
+             let .concat(_, _, sr),
+             let .shapeCast(_, _, sr),
+             let .typeCast(_, _, sr),
+             let .store(_, _, sr):
             return sr
         }
     }
 }
 
-enum OperandNode : ASTNode {
-    case immediate(ScalarType, Immediate, SourceRange)
-    case globalValue(DataType, String, SourceRange)
-    case temporary(DataType, String, SourceRange)
+enum ImmediateNode : ASTNode {
+    case bool(Bool, SourceRange), int(Int, SourceRange), float(Double, SourceRange)
 
-    var sourceRange: SourceRange {
+    var range: SourceRange {
         switch self {
-        case let .immediate(_, _, sr),
-             let .globalValue(_, _, sr),
-             let .temporary(_, _, sr):
+        case .bool(_, let sr), .int(_, let sr), .float(_, let sr):
             return sr
         }
     }
 }
+
+struct OperandNode : ASTNode {
+    let type: TypeNode
+    let shape: ShapeNode?
+    let variable: VariableNode
+
+    var range: SourceRange {
+        return type.range.lowerBound..<variable.range.upperBound
+    }
+}
+
+enum VariableNode : ASTNode {
+    case immediate(ImmediateNode, SourceRange)
+    case global(String, SourceRange)
+    case temporary(String, SourceRange)
+
+    var range: SourceRange {
+        switch self {
+        case let .immediate(_, sr), let .global(_, sr), let .temporary(_, sr):
+            return sr
+        }
+    }
+}
+
+enum TypeNode : ASTNode {
+    case bool(Int, SourceRange), int(Int, SourceRange), float(Int, SourceRange)
+
+    var range: SourceRange {
+        switch self {
+        case .bool(_, let sr), .int(_, let sr), .float(_, let sr):
+            return sr
+        }
+    }
+}
+
