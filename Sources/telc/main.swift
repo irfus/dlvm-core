530fcbcb8aab41fb84425a75f42d1253c1443439
Richard Wei <rxrwei@gmail.com>
2017-01-27 17:50:55 -0600

    Make telc output .dl file

--- Sources/telc/main.swift
+++ Sources/telc/main.swift
@@ -9,11 +9,14 @@ do {
     let telSource = try String(contentsOfFile: file, encoding: .utf8)
     print("Source file:", file)
     let ast = try ProgramTree.parser.parse(telSource)
-    print("Parse\n", ast)
     let program = try Program(parse: ast)
-    print("Type-checking passed")
     let module = program.makeModule()
-    print(module)
+    // Write to file
+    let url = URL(fileURLWithPath: file)
+    let dlUrl = url.deletingPathExtension().appendingPathExtension("dl")
+    var text = ""
+    module.write(to: &text)
+    try text.write(to: dlUrl, atomically: true, encoding: .utf8)
 }
 catch {
     print(error)
