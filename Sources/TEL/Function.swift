be8f3f102ad4e32c3b05004f89b9808c32f82eb9
Richard Wei <rxrwei@gmail.com>
2017-01-30 01:24:48 -0600

    Finish TEL function types

--- Sources/TEL/Function.swift
+++ Sources/TEL/Function.swift
@@ -8,103 +8,124 @@
 
 import DLVM
 
-public struct Function {
+public protocol FunctionProtocol {
+    var name: String { get }
+    var type: FunctionType { get }
+}
+
+public struct Function<IRFunction> : FunctionProtocol {
+    typealias Instruction = FunctionCallInstruction<IRFunction>
     public var name: String
     public var type: FunctionType
-    public var instructionType: DefiningInstruction.Type
+    public var irFunction: IRFunction
+}
+
+public extension FunctionProtocol {
+    public func makeInstruction(withArguments args: [Value],
+                                using builder: IRBuilder,
+                                name: String? = nil) -> DefiningInstruction {
+        switch self {
+        case let f as Function<AggregationFunction>:
+            return builder.makeAggregation(f.irFunction, args[0], name: name)
+        case let f as Function<ElementwiseFunction>:
+            return builder.makeElementwiseTransformation(f.irFunction, args[0], name: name)
+        case let f as Function<ArithmeticOperator>:
+            return builder.makeArithmeticOperation(f.irFunction, args[0], args[1], name: name)
+        case let f as Function<LogicOperator>:
+            return builder.makeLogicOperation(f.irFunction, args[0], args[1], name: name)
+        case let f as Function<ComparisonPredicate>:
+            return builder.makeComparison(f.irFunction, args[0], args[1], name: name)
+        case let f as Function<BinaryIntegrationFunction>:
+            return builder.makeBinaryReduction(f.irFunction, args[0], args[1], name: name)
+        default:
+            preconditionFailure("Unsupported function class \(type(of: self))")
+        }
+    }
 }
 
 /// - MARK: Helper factory functions
-extension Function {
-    
-    static func aggregation(_ name: String, function: AggregationFunction) -> Function {
-        return Function(name: name, type: .homomorphicUnary, instructionType: AggregationInstruction.self)
-    }
 
-    static func elementwise(_ name: String, function: ElementwiseFunction) -> Function {
-        return Function(name: name, type: .homomorphicUnary, instructionType: ElementwiseInstruction.self)
-    }
-
-    static func arithmetic(_ name: String, operator: ArithmeticOperator) -> Function {
-        return Function(name: name, type: .homomorphicBinary, instructionType: ArithmeticInstruction.self)
-    }
-    
-    static func logic(_ name: String, operator: LogicOperator) -> Function {
-        return Function(name: name, type: .homomorphicBinary, instructionType: LogicInstruction.self)
-    }
-    
-    static func comparison(_ name: String, predicate: ComparisonPredicate) -> Function {
-        return Function(name: name, type: .comparison, instructionType: ComparisonInstruction.self)
-    }
-    
-    static func binaryReduction(_ name: String, function: BinaryIntegrationFunction) -> Function {
-        return Function(name: name, type: .binaryReduction, instructionType: BinaryReductionInstruction.self)
-    }
-
-    static func matrixMultiplication(_ name: String) -> Function {
-        return Function(name: name, type: .matrixMultiplication, instructionType: MatrixMultiplicationInstruction.self)
-    }
-
-    static func tensorMultiplication(_ name: String) -> Function {
-        return Function(name: name, type: .tensorMultiplication, instructionType: TensorMultiplicationInstruction.self)
-    }
-    
+private func aggregation(_ name: String, function: AggregationFunction)
+    -> Function<AggregationFunction> {
+        return Function<AggregationFunction>(name: name, type: .homomorphicUnary, irFunction: function)
 }
 
-public let builtinFunctions: [Function] = [
+private func elementwise(_ name: String, function: ElementwiseFunction)
+    -> Function<ElementwiseFunction> {
+    return Function<ElementwiseFunction>(name: name, type: .homomorphicUnary, irFunction: function)
+}
+
+private func arithmetic(_ name: String, operator: ArithmeticOperator)
+    -> Function<ArithmeticOperator> {
+    return Function<ArithmeticOperator>(name: name, type: .homomorphicBinary, irFunction: `operator`)
+}
+
+private func logic(_ name: String, operator: LogicOperator)
+    -> Function<LogicOperator> {
+    return Function<LogicOperator>(name: name, type: .homomorphicBinary, irFunction: `operator`)
+}
+
+private func comparison(_ name: String, predicate: ComparisonPredicate)
+    -> Function<ComparisonPredicate> {
+    return Function<ComparisonPredicate>(name: name, type: .comparison, irFunction: predicate)
+}
+
+private func binaryReduction(_ name: String, function: BinaryIntegrationFunction)
+    -> Function<BinaryIntegrationFunction> {
+    return Function<BinaryIntegrationFunction>(name: name, type: .binaryReduction, irFunction: function)
+}
+
+/// - MARK: TEL built-in functions
+public let builtinFunctions: [FunctionProtocol] = [
     /// Elementwise tranfer functions
-    .elementwise("log", function: .log),
-    .elementwise("relu", function: .relu),
-    .elementwise("exp", function: .exp),
-    .elementwise("tan", function: .tan),
-    .elementwise("tanh", function: .tanh),
-    .elementwise("atan", function: .atan),
-    .elementwise("sin", function: .sin),
-    .elementwise("asin", function: .asin),
-    .elementwise("cos", function: .cos),
-    .elementwise("acos", function: .acos),
-    .elementwise("sqrt", function: .sqrt),
-    .elementwise("ceil", function: .ceil),
-    .elementwise("floor", function: .floor),
+    elementwise("sigmoid", function: .sigmoid),
+    elementwise("log", function: .log),
+    elementwise("relu", function: .relu),
+    elementwise("exp", function: .exp),
+    elementwise("tan", function: .tan),
+    elementwise("tanh", function: .tanh),
+    elementwise("atan", function: .atan),
+    elementwise("sin", function: .sin),
+    elementwise("asin", function: .asin),
+    elementwise("cos", function: .cos),
+    elementwise("acos", function: .acos),
+    elementwise("sqrt", function: .sqrt),
+    elementwise("ceil", function: .ceil),
+    elementwise("floor", function: .floor),
 
     /// Aggregate transfer functions
-    .aggregation("softmax", function: .softmax),
-    .aggregation("logSoftmax", function: .logSoftmax),
+    aggregation("softmax", function: .softmax),
+    aggregation("logSoftmax", function: .logSoftmax),
 
     /// Logical operators
-    .logic("&&", operator: .and),
-    .logic("||", operator: .or),
-    .logic("^^", operator: .xor),
+    logic("&&", operator: .and),
+    logic("||", operator: .or),
+    logic("^^", operator: .xor),
 
     /// Arithmetic operators
-    .arithmetic("+", operator: .add),
-    .arithmetic("-", operator: .subtract),
-    .arithmetic("*", operator: .multiply),
-    .arithmetic("/", operator: .divide),
-    .arithmetic("min", operator: .min),
-    .arithmetic("max", operator: .max),
-    .arithmetic("pow", operator: .power),
-    .arithmetic("mod", operator: .modulo),
+    arithmetic("+", operator: .add),
+    arithmetic("-", operator: .subtract),
+    arithmetic("*", operator: .multiply),
+    arithmetic("/", operator: .divide),
+    arithmetic("min", operator: .min),
+    arithmetic("max", operator: .max),
+    arithmetic("pow", operator: .power),
+    arithmetic("mod", operator: .modulo),
 
     /// Comparison operators
-    .comparison(">", predicate: .greaterThan),
-    .comparison("<", predicate: .lessThan),
-    .comparison(">=", predicate: .greaterThanOrEqualTo),
-    .comparison("<=", predicate: .lessThanOrEqualTo),
-    .comparison("==", predicate: .equalTo),
-    .comparison("!=", predicate: .notEqualTo),
-
-    /// Matrix multiplication operators (equivalent)
-    .matrixMultiplication("•"),
-    .matrixMultiplication("."),
-    .matrixMultiplication("⊗"),
+    comparison(">", predicate: .greaterThan),
+    comparison("<", predicate: .lessThan),
+    comparison(">=", predicate: .greaterThanOrEqualTo),
+    comparison("<=", predicate: .lessThanOrEqualTo),
+    comparison("==", predicate: .equalTo),
+    comparison("!=", predicate: .notEqualTo),
 
     /// Binary reduction functions
-    .binaryReduction("crossEntropy", function: .crossEntropy)
+    binaryReduction("crossEntropy", function: .crossEntropy)
 ]
 
-public let builtinFunctionTable: [String : Function] = {
-    var dict: [String : Function] = [:]
+public let builtinFunctionTable: [String : FunctionProtocol] = {
+    var dict: [String : FunctionProtocol] = [:]
     for function in builtinFunctions {
         dict[function.name] = function
     }
