2606bbe195ccf7ccc2d7f6382ea4f76881478069
Richard Wei <rxrwei@gmail.com>
2017-02-04 01:13:52 -0600

    Add DLVMRuntime target

--- Package.swift
+++ Package.swift
@@ -9,14 +9,16 @@ let package = Package(
         Target(name: "DLVMReader", dependencies: ["DLVM"]),
         /// DLVM compiler driver
         Target(name: "dlc", dependencies: ["DLVM", "DLVMReader"]),
+        /// DLVM runtime
+        Target(name: "DLVMRuntime", dependencies: ["DLVM", "DLVMReader"]),
         /// DLVM interpreter
-        Target(name: "dli", dependencies: ["DLVM", "DLVMReader"]),
+        Target(name: "dli", dependencies: ["DLVM", "DLVMReader", "DLVMRuntime"]),
         /// TEL compiler library (libTEL)
         Target(name: "TEL", dependencies: ["DLVM"]),
         /// TEL compiler driver
         Target(name: "telc", dependencies: ["DLVM", "TEL"])
     ],
     dependencies: [
-    	.Package(url: "https://github.com/rxwei/Parsey", majorVersion: 1, minor: 7)
+      	.Package(url: "https://github.com/rxwei/Parsey", majorVersion: 1, minor: 7)
     ]
 )
---
+++ Sources/DLVMRuntime/Tensor.swift
@@ -0,0 +1,9 @@
+//
+//  Tensor.swift
+//  DLVM
+//
+//  Created by Richard Wei on 2/3/17.
+//
+//
+
+import Foundation
