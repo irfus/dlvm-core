0ed5a39b8d4c0accbadaef7f7c32bdc544e19137
Richard Wei <rxrwei@gmail.com>
2017-02-05 02:16:10 -0600

    Add runtime tests

---
+++ Tests/DLVMRuntimeTests/DLVMRuntimeTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import DLVMRuntime
+
+class DLVMRuntimeTests: XCTestCase {
+
+    static var allTests : [(String, (DLVMRuntimeTests) -> () throws -> Void)] {
+        return [
+        ]
+    }
+    
+}
