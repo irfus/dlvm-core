4ee91fb83f0f7e1d047edb20eb40759fea89850f
Richard Wei <rxrwei@gmail.com>
2017-02-07 02:17:24 -0600

    Implement toolchain CLI

--- Sources/telc/main.swift
+++ Sources/telc/main.swift
@@ -1,23 +1,75 @@
 import TEL
 import Foundation
+import DLVMReader
+import CommandLineKit
 
-do {
-    guard let file = CommandLine.arguments.dropFirst().first else {
-        print("No file given")
-        exit(0)
+let cli = CommandLineKit.CommandLine()
+
+struct Options {
+    /// File
+    static let filePaths = MultiStringOption(shortFlag: "f", longFlag: "files",
+                                             helpMessage: "Paths to TEL source files")
+    /// Output
+    static let outputPaths = MultiStringOption(shortFlag: "o", longFlag: "outputs",
+                                               helpMessage: "Output file paths")
+    /// Help
+    static let needsHelp = BoolOption(shortFlag: "h", longFlag: "help",
+                                      helpMessage: "Print help message")
+}
+
+cli.addOptions(Options.filePaths,
+               Options.outputPaths,
+               Options.needsHelp)
+
+/// Parse command line
+do { try cli.parse(strict: true) }
+catch { cli.printUsage(error); exit(EX_USAGE) }
+
+func error(_ message: String) {
+    print("error: " + message)
+    exit(EX_USAGE)
+}
+
+func main() throws {
+
+    guard !Options.needsHelp.wasSet else {
+        print("Deep Learning Virtual Machine")
+        print("Tensor Expression Language Compiler\n")
+        cli.printUsage()
+        return
     }
-    let telSource = try String(contentsOfFile: file, encoding: .utf8)
-    print("Source file:", file)
-    let ast = try ProgramTree.parser.parse(telSource)
-    let program = try Program(parse: ast)
-    let module = program.makeModule()
-    // Write to file
-    let url = URL(fileURLWithPath: file)
-    let dlUrl = url.deletingPathExtension().appendingPathExtension("dl")
-    var text = ""
-    module.write(to: &text)
-    try text.write(to: dlUrl, atomically: true, encoding: .utf8)
+
+    guard let filePaths = Options.filePaths.value else {
+        error("no input files; use -f to specify files")
+        return
+    }
+
+    if let outputPaths = Options.outputPaths.value, outputPaths.count != filePaths.count {
+        error("different numbers of inputs and outputs specified")
+    }
+
+    let outputPaths = Options.outputPaths.value ?? filePaths.map { filePath in
+        let url = URL(fileURLWithPath: filePath)
+        let dlUrl = url.deletingPathExtension().appendingPathExtension("dl")
+        return dlUrl.relativePath
+    }
+
+    for (filePath, outputPath) in zip(filePaths, outputPaths) {
+        let source = try String(contentsOfFile: filePath)
+        print("Source file:", filePath)
+        let ast = try ProgramTree.parser.parse(source)
+        let program = try Program(parse: ast)
+        print("TEL module \"\(program.moduleName)\"")
+        /// Generate IR
+        let module = program.makeModule()
+        try module.verify()
+        /// Write IR
+        FileManager.default.createFile(atPath: outputPath, contents: nil, attributes: nil)
+        try "\(module)".write(toFile: outputPath, atomically: true, encoding: .utf8)
+        print("DLVM module \"\(module.name)\" written to \(outputPath)")
+    }
+
 }
-catch {
-    print(error)
-}
+
+do { try main() }
+catch { print(error) }
