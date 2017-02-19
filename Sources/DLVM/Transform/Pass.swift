6f20a0e7487064fbd5708ad669b69566f1592552
Richard Wei <rxrwei@gmail.com>
2017-02-19 15:42:36 -0600

    [Pass] Clean up

--- Sources/DLVM/Transform/Pass.swift
+++ Sources/DLVM/Transform/Pass.swift
@@ -6,46 +6,20 @@
 //
 //
 
-//public protocol PassResultProtocol {
-//    var changed: Bool { get }
-//    init()
-//}
-//
-//public struct PassResult : PassResultProtocol {
-//    public var changed: Bool = false
-//    public init() {}
-//}
-//
-//public protocol Pass : class, SelfVerifiable {
-//    associatedtype Result : PassResultProtocol
-//    associatedtype Body : AnyObject
-//    var body: Body { get }
-//    func run() -> Result
-//    init(body: Body)
-//}
-//
-//public extension Pass where Body : Global {
-//    var module : Module {
-//        guard let module = body.module else {
-//            preconditionFailure("Module not available")
-//        }
-//        return module
-//    }
-//}
-//
-//public protocol BasicBlockPass : Pass {
-//    typealias Body = BasicBlock
-//}
-//
-//public protocol BasicBlockExtensionPass : BasicBlockPass {
-//    static var extensionType: BasicBlock.ExtensionType { get }
-//}
-//
-//public extension Pass where Body : Global {
-//    public func verify() throws {
-//        guard let module = body.module else {
-//            throw VerificationError.globalMissingParent(body)
-//        }
-//        try module.verify()
-//    }
-//}
+public protocol PassResultProtocol {
+    var changed: Bool { get }
+    init()
+}
+
+public struct PassResult : PassResultProtocol {
+    public var changed: Bool = false
+    public init() {}
+}
+
+public protocol Pass : class, SelfVerifiable {
+    associatedtype Result : PassResultProtocol
+    associatedtype Body : AnyObject
+    var body: Body { get }
+    func run() -> Result
+    init(body: Body)
+}
