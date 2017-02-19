0d76cb296b9a5539373f9842941e1a392581886f
Richard Wei <rxrwei@gmail.com>
2017-02-19 15:42:36 -0600

    [Pass] Clean up

--- Sources/DLVM/Transform/Backpropagation.swift
+++ Sources/DLVM/Transform/Backpropagation.swift
@@ -4,31 +4,4 @@
 //
 //  Created by Richard Wei on 2/7/17.
 //
-//
-//
-//open class BackpropagationPass<Loss, Opt, AutoDiff> : BasicBlockExtensionPass
-//    where Loss : LossFunctionPass,
-//          Opt : OptimizerPass,
-//          AutoDiff : AutomaticDifferentiationPass
-//{
-//
-//    public var body: BasicBlock
-//
-//    lazy var lossFunctionPass: Loss = Loss(body: self.body)
-//    lazy var autoDiffPass: AutoDiff = AutoDiff(body: self.body)
-//    lazy var optimizerPass: Opt = Opt(body: self.body)
-//
-//
-//    public static var extensionType: BasicBlock.ExtensionType {
-//        return .backpropagation
-//    }
-//
-//    public required init(body: BasicBlock) {
-//        self.body = body
-//    }
-//
-//    public func run() -> PassResult {
-//        return PassResult()
-//    }
-//    
-//}
+
