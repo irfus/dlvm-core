5c2e8e3b38f589f736fabd7ca51ddd9a1e7e6425
Richard Wei <rxrwei@gmail.com>
2017-02-26 02:13:05 -0600

    Implement transpose graph iterators

--- Sources/DLVM/ADT/Iterators.swift
+++ Sources/DLVM/ADT/Iterators.swift
@@ -6,19 +6,21 @@
 //
 //
 
+import class Foundation.NSMutableSet
+
+public enum TraversalOrder {
+    case preorder, postorder, breadthFirst
+}
+
 public struct GraphIterator<Node : GraphNode> : IteratorProtocol
     where Node.SuccessorSequence.Iterator.Element == Node {
 
-    public enum Order {
-        case preorder, postorder, breadthFirst
-    }
-
     private var pre: [Node] = []
     private var post: [Node] = []
-    private var visited: ObjectSet<Node> = []
-    public let order: Order
+    private var visited: NSMutableSet = []
+    public let order: TraversalOrder
 
-    public init(root: Node?, order: Order) {
+    public init(root: Node?, order: TraversalOrder) {
         if let root = root {
             pre.append(root)
         }
@@ -33,7 +35,7 @@ public struct GraphIterator<Node : GraphNode> : IteratorProtocol
             for child in node.successors where !visited.contains(child) {
                 pre.append(child)
             }
-            visited.insert(node)
+            visited.add(node)
             return node
 
         case .preorder:
@@ -42,7 +44,7 @@ public struct GraphIterator<Node : GraphNode> : IteratorProtocol
             for child in node.successors.reversed() where !visited.contains(child) {
                 pre.append(child)
             }
-            visited.insert(node)
+            visited.add(node)
             return node
 
         case .postorder:
@@ -52,9 +54,83 @@ public struct GraphIterator<Node : GraphNode> : IteratorProtocol
                 pre.append(child)
             }
             post.append(node)
-            visited.insert(node)
+            visited.add(node)
             return next()
         }
     }
 }
 
+public struct TransposeGraphIterator<Node : BidirectionalGraphNode> : IteratorProtocol
+    where Node.SuccessorSequence.Iterator.Element == Node,
+          Node.PredecessorSequence.Iterator.Element == Node {
+
+    private var pre: [Node] = []
+    private var post: [Node] = []
+    private var visited: NSMutableSet = []
+    public let order: TraversalOrder
+
+    public init(root: Node?, order: TraversalOrder) {
+        if let root = root {
+            pre.append(root)
+        }
+        self.order = order
+    }
+
+    public mutating func next() -> Node? {
+        switch order {
+        case .breadthFirst:
+            if pre.isEmpty { return nil }
+            let node = pre.removeFirst()
+            for child in node.predecessors where !visited.contains(child) {
+                pre.append(child)
+            }
+            visited.add(node)
+            return node
+
+        case .preorder:
+            if pre.isEmpty { return nil }
+            let node = pre.removeLast()
+            for child in node.predecessors.reversed() where !visited.contains(child) {
+                pre.append(child)
+            }
+            visited.add(node)
+            return node
+
+        case .postorder:
+            if pre.isEmpty { return post.popLast() }
+            let node = pre.removeLast()
+            for child in node.predecessors where !visited.contains(child) {
+                pre.append(child)
+            }
+            post.append(node)
+            visited.add(node)
+            return next()
+        }
+    }
+    
+}
+
+public extension GraphNode where SuccessorSequence.Iterator.Element == Self {
+    func traversed(in order: TraversalOrder) -> IteratorSequence<GraphIterator<Self>> {
+        return IteratorSequence(GraphIterator(root: self, order: order))
+    }
+
+    var preorder: IteratorSequence<GraphIterator<Self>> {
+        return traversed(in: .preorder)
+    }
+
+    var postorder: IteratorSequence<GraphIterator<Self>> {
+        return traversed(in: .postorder)
+    }
+
+    var breadthFirst: IteratorSequence<GraphIterator<Self>> {
+        return traversed(in: .breadthFirst)
+    }
+}
+
+public extension BidirectionalGraphNode where SuccessorSequence.Iterator.Element == Self,
+                                              PredecessorSequence.Iterator.Element == Self {
+    func transposeTraversed(in order: TraversalOrder) -> IteratorSequence<TransposeGraphIterator<Self>> {
+        return IteratorSequence(TransposeGraphIterator(root: self, order: order))
+    }
+}
