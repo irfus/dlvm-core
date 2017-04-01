39bb23fc09b6df143cc4b415c2f50547530d0a94
Richard Wei <rxrwei@gmail.com>
2017-04-01 18:27:56 -0500

    Add 'dlrt'

---
+++ Sources/dlrt/include/ref.h
@@ -0,0 +1,32 @@
+//
+//  ref.h
+//  DLVM
+//
+//  Created by Richard Wei on 3/28/17.
+//
+//
+
+#import <stdatomic.h>
+
+/**
+ Reference
+ */
+struct _dl_ref {
+    void (* __nonnull free)(const struct _dl_ref * const __nonnull);
+    _Atomic long count;
+} __attribute__((swift_name("DLReference")));
+
+struct _dl_ref _dl_ref_init(void (* __nonnull free)(const struct _dl_ref * const __nonnull))
+    __attribute__((swift_name("DLReference.init(free:)")));
+
+void _dl_ref_retain(struct _dl_ref * const __nonnull ref)
+    __attribute__((swift_name("DLReference.retain(self:)")));
+
+void _dl_ref_release(struct _dl_ref * const __nonnull ref)
+    __attribute__((swift_name("DLReference.release(self:)")));
+
+void _dl_ref_dealloc(struct _dl_ref * const __nonnull ref)
+    __attribute__((swift_name("DLReference.deallocate(self:)")));
+
+long _dl_ref_count(const struct _dl_ref * const __nonnull ref)
+    __attribute__((swift_name("getter:DLReference.count(self:)")));
