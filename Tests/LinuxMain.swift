6d7dd4254dc32eead49591b8df6758d150fb5ef2
Richard Wei <rxrwei@gmail.com>
2017-04-03 15:21:16 -0500

    DLVMRuntime be gone

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,10 +1,8 @@
 import XCTest
 @testable import DLVMTests
-@testable import DLVMRuntimeTests
 @testable import TELTests
 
 XCTMain([
      testCase(DLVMTests.allTests),
-     testCase(DLVMRuntimeTests.allTests),
      testCase(TELTests.allTests),
 ])
