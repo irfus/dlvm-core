0fd9214aa2c6a8e6cc0c982454b19ca497e24123
Richard Wei <rxrwei@gmail.com>
2017-04-03 02:05:22 -0500

    Totally uninteresting fix: `__nonnull` -> `_Nonnull`

--- Sources/dlrt/include/ref.h
+++ Sources/dlrt/include/ref.h
@@ -12,21 +12,21 @@
  Reference
  */
 struct _dl_ref {
-    void (* __nonnull free)(const struct _dl_ref * const __nonnull);
+    void (* _Nonnull free)(const struct _dl_ref * const _Nonnull);
     _Atomic long count;
 } __attribute__((swift_name("DLReference")));
 
-struct _dl_ref _dl_ref_init(void (* __nonnull free)(const struct _dl_ref * const __nonnull))
+struct _dl_ref _dl_ref_init(void (* __nonnull free)(const struct _dl_ref * const _Nonnull))
     __attribute__((swift_name("DLReference.init(free:)")));
 
-void _dl_ref_retain(struct _dl_ref * const __nonnull ref)
+void _dl_ref_retain(struct _dl_ref * const _Nonnull ref)
     __attribute__((swift_name("DLReference.retain(self:)")));
 
-void _dl_ref_release(struct _dl_ref * const __nonnull ref)
+void _dl_ref_release(struct _dl_ref * const _Nonnull ref)
     __attribute__((swift_name("DLReference.release(self:)")));
 
-void _dl_ref_dealloc(struct _dl_ref * const __nonnull ref)
+void _dl_ref_dealloc(struct _dl_ref * const _Nonnull ref)
     __attribute__((swift_name("DLReference.deallocate(self:)")));
 
-long _dl_ref_count(const struct _dl_ref * const __nonnull ref)
+long _dl_ref_count(const struct _dl_ref * const _Nonnull ref)
     __attribute__((swift_name("getter:DLReference.count(self:)")));
--- Sources/dlrt/ref.c
+++ Sources/dlrt/ref.c
@@ -8,27 +8,27 @@
 
 #import "ref.h"
 
-struct _dl_ref _dl_ref_init(void (* __nonnull free)(const struct _dl_ref * const __nonnull)) {
+struct _dl_ref _dl_ref_init(void (* _Nonnull free)(const struct _dl_ref * const _Nonnull)) {
     return (struct _dl_ref){ free, 1 };
 }
 
-void _dl_ref_retain(struct _dl_ref *const __nonnull ref)
+void _dl_ref_retain(struct _dl_ref *const _Nonnull ref)
 {
     atomic_fetch_add((_Atomic int *)&ref->count, 1);
 }
 
-void _dl_ref_release(struct _dl_ref *const __nonnull ref)
+void _dl_ref_release(struct _dl_ref *const _Nonnull ref)
 {
     if (atomic_fetch_sub((_Atomic int *)&ref->count, 1) == 1)
         ref->free(ref);
 }
 
-void _dl_ref_dealloc(struct _dl_ref *const __nonnull ref)
+void _dl_ref_dealloc(struct _dl_ref *const _Nonnull ref)
 {
     ref->free(ref);
 }
 
-long _dl_ref_count(const struct _dl_ref *const __nonnull ref)
+long _dl_ref_count(const struct _dl_ref *const _Nonnull ref)
 {
     return ref->count;
 }
