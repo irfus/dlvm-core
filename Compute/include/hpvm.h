20efad2527e1842ed36855bf9fd2c59c812fc355
Richard Wei <rxrwei@gmail.com>
2017-04-12 22:45:12 -0500

    Use non-variadic attribute functions

--- Compute/include/hpvm.h
+++ Compute/include/hpvm.h
@@ -36,25 +36,26 @@ typedef SWIFT_ENUM_NAMED(int, Target, "HPVM.Target") {
 void __hpvm__hint(Target) SWIFT_NAME(HPVM.hint(_:));
 void __hpvm__wait(int);
 
-void __attribute__((noinline)) __hpvm__attributes(int, ...);
+void __hpvm__attribute_in(const void *_Nonnull) SWIFT_NAME(HPVM.attributeIn(_:));
+void __hpvm__attribute_out(const void *_Nonnull) SWIFT_NAME(HPVM.attributeOut(_:));
 void __hpvm__init(); // Separately wrapped in hpvm.swift
 void __hpvm__cleanup(); // Separately wrapped in hpvm.swift
-void __hpvm__bindIn(void *_Nonnull, int, int, int) SWIFT_NAME(HPVM.bindIn(_:_:_:_:));
-void __hpvm__bindOut(void *_Nonnull, int, int, int) SWIFT_NAME(HPVM.bindOut(_:_:_:_:));
-void *_Nonnull __hpvm__edge(void *_Nonnull, void *_Nonnull, int, int, int, int) SWIFT_NAME(HPVM.bindOut(_:_:_:_:_:_:));
-void __hpvm__push(void *_Nonnull, void *_Nonnull) SWIFT_NAME(HPVM.push(_:_:));
-void *_Nonnull __hpvm__pop(void *_Nonnull) SWIFT_NAME(HPVM.pop(_:));
-void *_Nonnull __attribute__((noinline)) __hpvm__launch(int, ...);
-void *_Nonnull __hpvm__getNode(); // Separately wrapped in hpvm.swift
-void *_Nonnull __hpvm__getParentNode(void *_Nonnull) SWIFT_NAME(HPVM.parentNode(of:));
+void __hpvm__bindIn(const void *_Nonnull, int, int, int) SWIFT_NAME(HPVM.bindIn(_:_:_:_:));
+void __hpvm__bindOut(const void *_Nonnull, int, int, int) SWIFT_NAME(HPVM.bindOut(_:_:_:_:));
+const void *_Nonnull __hpvm__edge(const void *_Nonnull, const void *_Nonnull, int, int, int, int) SWIFT_NAME(HPVM.bindOut(_:_:_:_:_:_:));
+void __hpvm__push(const void *_Nonnull, const void *_Nonnull) SWIFT_NAME(HPVM.push(_:_:));
+const void *_Nonnull __hpvm__pop(const void *_Nonnull) SWIFT_NAME(HPVM.pop(_:));
+const void *_Nonnull __hpvm__launch(int, ...);
+const void *_Nonnull __hpvm__getNode(); // Separately wrapped in hpvm.swift
+const void *_Nonnull __hpvm__getParentNode(const void *_Nonnull) SWIFT_NAME(HPVM.parentNode(of:));
 void __hpvm__barrier(); // Separately wrapped in hpvm.swift
-void *_Nonnull __hpvm__malloc(long) SWIFT_NAME(HPVM.malloc(_:));
-int __hpvm__getNodeInstanceID_x(void *_Nonnull) SWIFT_NAME(HPVM.nodeInstanceIdX(of:));
-int __hpvm__getNodeInstanceID_y(void *_Nonnull) SWIFT_NAME(HPVM.nodeInstanceIdY(of:));
-int __hpvm__getNodeInstanceID_z(void *_Nonnull) SWIFT_NAME(HPVM.nodeInstanceIdZ(of:));
-int __hpvm__getNumNodeInstances_x(void *_Nonnull) SWIFT_NAME(HPVM.nodeInstanceCountX(of:));
-int __hpvm__getNumNodeInstances_y(void *_Nonnull) SWIFT_NAME(HPVM.nodeInstanceCountY(of:));
-int __hpvm__getNumNodeInstances_z(void *_Nonnull) SWIFT_NAME(HPVM.nodeInstanceCountZ(of:));
+const void *_Nonnull __hpvm__malloc(long) SWIFT_NAME(HPVM.malloc(_:));
+int __hpvm__getNodeInstanceID_x(const void *_Nonnull) SWIFT_NAME(HPVM.nodeInstanceIdX(of:));
+int __hpvm__getNodeInstanceID_y(const void *_Nonnull) SWIFT_NAME(HPVM.nodeInstanceIdY(of:));
+int __hpvm__getNodeInstanceID_z(const void *_Nonnull) SWIFT_NAME(HPVM.nodeInstanceIdZ(of:));
+int __hpvm__getNumNodeInstances_x(const void *_Nonnull) SWIFT_NAME(HPVM.nodeInstanceCountX(of:));
+int __hpvm__getNumNodeInstances_y(const void *_Nonnull) SWIFT_NAME(HPVM.nodeInstanceCountY(of:));
+int __hpvm__getNumNodeInstances_z(const void *_Nonnull) SWIFT_NAME(HPVM.nodeInstanceCountZ(of:));
 
 // Atomic
 int __hpvm__atomic_cmpxchg(int *_Nonnull, int, int) SWIFT_NAME(HPVMAtomic.cmpxchg(_:_:_:));
@@ -80,20 +81,3 @@ float __hpvm__sin(float) SWIFT_NAME(HPVM.sin(_:));
 void llvm_hpvm_track_mem(void *_Nonnull, size_t) SWIFT_NAME(HPVMRuntime.trackMemory(_:ofSize:));
 void llvm_hpvm_untrack_mem(void *_Nonnull) SWIFT_NAME(HPVMRuntime.untrackMemory(_:));
 void llvm_hpvm_request_mem(void *_Nonnull, size_t) SWIFT_NAME(HPVMRuntime.requestMemory(_:ofSize:));
-
-
-/// Specialize and overload variadics since Swift Clang importer does not natively support variadics
-
-inline void __hpvm__attributes_3_1(int, const void *_Nonnull, const void *_Nonnull, const void *_Nonnull, int, const void *_Nonnull)
-    SWIFT_NAME(HPVM.attributes(_:_:_:_:_:_:));
-
-inline void __hpvm__attributes_3_1(int a1, const void *_Nonnull a2, const void *_Nonnull a3, const void *_Nonnull a4,
-                                   int a5, const void *_Nonnull a6) {
-    __hpvm__attributes(a1, a2, a3, a4, a5, a6);
-}
-
-inline void __hpvm__launch_1(int) SWIFT_NAME(HPVM.launch(_:));
-
-inline void __hpvm__launch_1(int a1) {
-    __hpvm__launch(a1);
-}
