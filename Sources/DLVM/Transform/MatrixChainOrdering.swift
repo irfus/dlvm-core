75f4016a33c2fd081cc4a8328f9b1ce110261d64
Richard Wei <rxrwei@gmail.com>
2017-05-22 23:32:21 +0800

    Add MMOrdering, LSFusion and AlgSimp passes

---
+++ Sources/DLVM/Transform/MatrixChainOrdering.swift
@@ -0,0 +1,14 @@
+//
+//  MatrixChainOrdering.swift
+//  DLVM
+//
+//  Created by Richard Wei on 5/22/17.
+//
+//
+
+open class MatrixChainOrdering : TransformPass {
+    public typealias Body = BasicBlock
+    open class func run(on body: BasicBlock) throws -> Bool {
+        DLUnimplemented()
+    }
+}
