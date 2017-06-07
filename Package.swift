19ffdfba08d4f6ad3ffd4f104033764bc9dbd4a6
Will Dietz <w@wdtz.org>
2017-06-07 17:53:23 -0500

    dlc: Add missing dep on DLVMCodeGen

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         Target(name: "DLVM"),
         Target(name: "DLVMCodeGen", dependencies: ["DLVM"]),
         Target(name: "DLParse", dependencies: ["DLVM"]),
-        Target(name: "dlc", dependencies: ["DLVM", "DLParse"])
+        Target(name: "dlc", dependencies: ["DLVM", "DLVMCodeGen", "DLParse"])
     ],
     dependencies: [
       	.Package(url: "https://github.com/rxwei/LLVM_C", majorVersion: 2, minor: 1),
