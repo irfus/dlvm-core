8439d7bf3b652c3dc84d22e93500c6821ac119e4
Richard Wei <rxrwei@gmail.com>
2017-07-11 18:37:18 +0800

    [Transform] Add much more patterns to AlgebraSimplification
    * Separated patterns to four categories: arithmetics, trigonometry, reassociation, and linear algebra
    
    * Inspired by XLA's AS pass: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/compiler/xla/service/algebraic_simplifier.cc

--- Sources/DLVM/Transform/AlgebraSimplification.swift
+++ Sources/DLVM/Transform/AlgebraSimplification.swift
@@ -17,20 +17,29 @@
 //  limitations under the License.
 //
 
-import Foundation
-
 /// Algebra Simplification simplifies the following expressions
-/// - x^(-1) => 1 / x
-/// - x^0 => 1
-/// - x^1 => x
-/// - x^2 => x * x
-/// - (e^x - e^(-x)) / 2 => sinh(x)
-/// - (e^x + e^(-x)) / 2 => cosh(x)
-/// - (e^x - e^(-x)) / (e^x + e^(-x)) => tanh(x)
-/// - sin(0) | sinh(0) => 0
-/// - cos(0) | cosh(0) => 1
-/// - tan(0) | tanh(0) => 0
-/// - (e^x)^y => e^(x*y)
+/// 1. Arithmetics
+///    - x + 0 | 0 + x | x - 0 | x * 1 | 1 * x | x / 1 => x
+///    - x * 0 | 0 * x => 0
+///    - x^(-1) => 1 / x
+///    - x^0 => 1
+///    - x^1 => x
+///    - x^2 => x * x
+/// 2. Trignometry
+///    - (e^x - e^(-x)) / 2 => sinh(x)
+///    - (e^x + e^(-x)) / 2 => cosh(x)
+///    - (e^x - e^(-x)) / (e^x + e^(-x)) => tanh(x)
+///    - sin(0) | sinh(0) => 0
+///    - cos(0) | cosh(0) => 1
+///    - tan(0) | tanh(0) => 0
+/// 3. Reassociation
+///    - (e^x)^y => e^(x*y)
+///    - e^x / e^y => e^(x-y)
+///    - (x / y) / (z / a) => (x * a) / (y * z)
+///    - (x / y) / z => x / (y * z)
+///    - x / (y / z) => (x * z) / y
+/// 4. Linear algebra
+///    - (A^T)^T => A
 open class AlgebraSimplification : TransformPass {
     public typealias Body = Function
 
@@ -41,6 +50,22 @@ open class AlgebraSimplification : TransformPass {
                                               workList: inout [AlgebraicExpression]) -> Bool {
         /// Pattern-match expressions
         switch expr {
+        // MARK: - 1. Arithmetics
+        /// - x + 0 | 0 + x | x - 0 | x * 1 | 1 * x => x
+        case let .zipWith(.associative(.add), x, 0, inst),
+             let .zipWith(.associative(.add), 0, x, inst),
+             let .zipWith(.associative(.subtract), x, 0, inst),
+             let .zipWith(.associative(.multiply), x, 1, inst),
+             let .zipWith(.associative(.multiply), 1, x, inst),
+             let .zipWith(.associative(.divide), x, 1, inst):
+            function.replaceAllUses(of: inst, with: %x)
+            expr.removeIntermediates(upTo: x)
+            workList.append(x)
+        /// - x * 0 | 0 * x => 0
+        case let .zipWith(.associative(.multiply), x, 0, inst),
+             let .zipWith(.associative(.multiply), 0, x, inst):
+            function.replaceAllUses(of: inst, with: %x.makeLiteral(0))
+            expr.removeIntermediates(upTo: x)
         /// - x^(-1) => 1 / x
         case let .zipWith(.associative(.power), x, -1, inst):
             builder.move(after: inst)
@@ -54,10 +79,9 @@ open class AlgebraSimplification : TransformPass {
             let newVal = expr.makeLiteral(1)
             function.replaceAllUses(of: inst, with: %newVal)
             expr.removeIntermediates(upTo: x)
-            workList.append(x)
         /// - x^1 => x
         case let .zipWith(.associative(.power), x, 1, inst):
-            function.replaceAllUses(of: inst, with: x.value.makeUse())
+            function.replaceAllUses(of: inst, with: %x)
             expr.removeIntermediates(upTo: x)
             workList.append(x)
         /// - x^2 => x * x
@@ -67,6 +91,8 @@ open class AlgebraSimplification : TransformPass {
             function.replaceAllUses(of: inst, with: %product)
             expr.removeIntermediates(upTo: x)
             workList.append(.zipWith(.associative(.multiply), x, x, product))
+
+        // MARK: - 2. Trignometry
         /// - (e^x - e^(-x)) / 2 => sinh(x)
         case let .zipWith(.associative(.divide),
                           .zipWith(.associative(.subtract),
@@ -101,6 +127,8 @@ open class AlgebraSimplification : TransformPass {
              let .map(.cosh, 0, inst):
             function.replaceAllUses(of: inst, with: %expr.makeLiteral(1))
             expr.removeIntermediates()
+
+        // MARK: - 3. Reassociation
         /// - (e^x)^y => e^(x*y)
         case let .zipWith(.associative(.power), .map(.exp, x, _), y, inst):
             builder.move(after: inst)
@@ -109,7 +137,50 @@ open class AlgebraSimplification : TransformPass {
             function.replaceAllUses(of: inst, with: %exp)
             expr.removeIntermediates(upTo: x, y)
             workList.append(.map(.exp, .zipWith(.associative(.multiply), x, y, mul), exp))
-        /// (x / y) / z => x / (y * z)
+        /// - e^x / e^y => e^(x-y)
+        case let .zipWith(.associative(.divide), .map(.exp, x, _), .map(.exp, y, _), inst):
+            builder.move(after: inst)
+            let sub = builder.subtract(%x, %y)
+            let exp = builder.exp(%sub)
+            function.replaceAllUses(of: inst, with: %exp)
+            expr.removeIntermediates(upTo: x, y)
+            workList.append(.map(.exp, .zipWith(.associative(.subtract), x, y, sub), exp))
+        /// - x / e^y => x * e^(-y)
+        case let .zipWith(.associative(.divide), x, .map(.exp, y, _), inst):
+            builder.move(after: inst)
+            let neg = builder.map(.negate, %y)
+            let exp = builder.exp(%neg)
+            let mul = builder.multiply(%x, %exp)
+            function.replaceAllUses(of: inst, with: %mul)
+            expr.removeIntermediates(upTo: x, y)
+            workList.append(.zipWith(.associative(.multiply), x,
+                                     .map(.exp, .map(.negate, y, neg), exp), mul))
+        /// - x / y^z => x * y^(-z)
+        case let .zipWith(.associative(.divide), x, .zipWith(.associative(.power), y, z, _), inst):
+            builder.move(after: inst)
+            let neg = builder.map(.negate, %z)
+            let pwr = builder.power(%y, %neg)
+            let mul = builder.multiply(%x, %pwr)
+            function.replaceAllUses(of: inst, with: %mul)
+            expr.removeIntermediates(upTo: x, y, z)
+            workList.append(.zipWith(.associative(.multiply), x,
+                                     .zipWith(.associative(.power), y,
+                                              .map(.negate, z, neg), pwr), mul))
+        /// - (x / y) / (z / a) => (x * a) / (y * z)
+        case let .zipWith(.associative(.divide),
+                          .zipWith(.associative(.divide), x, y, _),
+                          .zipWith(.associative(.divide), z, a, _), inst):
+            builder.move(after: inst)
+            let mulL = builder.multiply(%x, %a)
+            let mulR = builder.multiply(%y, %z)
+            let div = builder.divide(%mulL, %mulR)
+            function.replaceAllUses(of: inst, with: %div)
+            expr.removeIntermediates(upTo: x, y, z, a)
+            workList.append(.zipWith(.associative(.divide),
+                                     .zipWith(.associative(.multiply), x, a, mulL),
+                                     .zipWith(.associative(.multiply), y, z, mulR),
+                                     div))
+        /// - (x / y) / z => x / (y * z)
         case let .zipWith(.associative(.divide),
                           .zipWith(.associative(.divide), x, y, _),
                           z, inst):
@@ -121,11 +192,31 @@ open class AlgebraSimplification : TransformPass {
             workList.append(.zipWith(.associative(.divide), x,
                                      .zipWith(.associative(.multiply), y, z, mul),
                                      div))
+        /// - x / (y / z) => (x * z) / y
+        case let .zipWith(.associative(.divide), x, .zipWith(.associative(.divide), y, z, _), inst):
+            builder.move(after: inst)
+            let mul = builder.multiply(%x, %z)
+            let div = builder.divide(%mul, %y)
+            function.replaceAllUses(of: inst, with: %div)
+            expr.removeIntermediates(upTo: x, y, z)
+            workList.append(.zipWith(.associative(.divide),
+                                     .zipWith(.associative(.multiply), x, z, mul), y, div))
+
+        // MARK: - 4. Linear Algebra
+        /// - (A^T)^T => A
+        case let .transpose(.transpose(A, _), inst):
+            builder.move(after: inst)
+            function.replaceAllUses(of: inst, with: %A)
+            expr.removeIntermediates(upTo: A)
+            workList.append(A)
+
         default:
             return false
         }
         return true
     }
+
+    // MARK: - Pass entry
 
     open class func run(on body: Function) throws -> Bool {
         var changed = false
