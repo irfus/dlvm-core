f594cb91407d71efe9bc6c1245256772c360dfd6
Richard Wei <rxwei@google.com>
2017-12-22 03:44:37 -0800

    [Runtime] Add AD stack

---
+++ Runtime/src/ad_stack.cpp
@@ -0,0 +1,44 @@
+//
+//  ad_stack.cpp
+//  DLVM
+//
+//  Copyright 2016-2017 DLVM Team.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#include "ad_stack.h"
+
+DLADStack * _Nonnull DLADStackCreate() {
+    return new DLADStack;
+}
+
+void DLADStackDestroy(DLADStack * _Nonnull stack) {
+    delete stack;
+}
+
+void DLADStackPush(DLADStack * _Nonnull stack, DLValue value) {
+    stack->push(value);
+}
+
+void DLADStackPop(DLADStack * _Nonnull stack) {
+    stack->pop();
+}
+
+DLValue DLADStackTop(DLADStack * _Nonnull stack) {
+    return stack->top();
+}
+
+size_t DLADStackSize(DLADStack * _Nonnull stack) {
+    return stack->size();
+}
