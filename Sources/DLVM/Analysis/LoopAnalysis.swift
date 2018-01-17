edb94f9a74cc9dc7b78d7d36782b482823e2c181
Richard Wei <rxwei@google.com>
2018-01-17 00:04:40 -0800

    [Analysis] Make loop fields public

--- Sources/DLVM/Analysis/LoopAnalysis.swift
+++ Sources/DLVM/Analysis/LoopAnalysis.swift
@@ -27,10 +27,10 @@
 /// contained entirely within the loop and the basic blocks that make up the
 /// loop.
 
-public class Loop : EquatableByReference {
-    public fileprivate(set) weak var parent: Loop?
-    public fileprivate(set) var subloops: [Loop] = []
-    public fileprivate(set) var blocks: OrderedSet<BasicBlock>
+public class Loop : HashableByReference {
+    public weak var parent: Loop?
+    public var subloops: [Loop] = []
+    public var blocks: OrderedSet<BasicBlock>
 
     public init(parent: Loop? = nil, header: BasicBlock) {
         self.parent = parent
@@ -146,8 +146,14 @@ public extension Loop {
 }
 
 public struct LoopInfo {
-    public fileprivate(set) var innerMostLoops: [BasicBlock : Loop] = [:]
-    public fileprivate(set) var topLevelLoops: [Loop] = []
+    public var innerMostLoops: [BasicBlock : Loop] = [:]
+    public var topLevelLoops: [Loop] = []
+}
+
+public extension LoopInfo {
+    var isEmpty: Bool {
+        return topLevelLoops.isEmpty
+    }
 }
 
 /// Analyze LoopInfo discovers loops during a postorder DominatorTree traversal
