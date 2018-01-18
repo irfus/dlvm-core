9c51e06a10117c10c61ea88dd1b143912c7dacea
Dan Zheng <zheng321@purdue.edu>
2018-01-18 07:14:01 -0500

    [CLT] [FileCheck] Add CFGCanonicalization pass option and test

--- Sources/DLCommandLineTools/Passes.swift
+++ Sources/DLCommandLineTools/Passes.swift
@@ -22,46 +22,49 @@ import protocol Utility.StringEnumArgument
 import enum Utility.ShellCompletion
 
 public enum TransformPass: String {
-    case differentiation = "Differentiation"
-    case deadCodeElimination = "DeadCodeElimination"
-    case commonSubexpressionElimination = "CommonSubexpressionElimination"
     case algebraSimplification = "AlgebraSimplification"
+    case cfgCanonicalization = "CFGCanonicalization"
+    case commonSubexpressionElimination = "CommonSubexpressionElimination"
+    case deadCodeElimination = "DeadCodeElimination"
+    case differentiation = "Differentiation"
     case linearAlgebraFusion = "LinearAlgebraFusion"
+    case literalBroadcastingPromotion = "LiteralBroadcastingPromotion"
+    case matrixMultiplicationReordering = "MatrixMultiplicationReordering"
     case stackPromotion = "StackPromotion"
     case valuePromotion = "ValuePromotion"
-    case matrixMultiplicationReordering = "MatrixMultiplicationReordering"
-    case literalBroadcastingPromotion = "LiteralBroadcastingPromotion"
 }
 
 public extension TransformPass {
     var abbrevation: String {
         switch self {
-        case .differentiation: return "AD"
-        case .deadCodeElimination: return "DCE"
-        case .commonSubexpressionElimination: return "CSE"
         case .algebraSimplification: return "AS"
+        case .cfgCanonicalization: return "CFGCan"
+        case .commonSubexpressionElimination: return "CSE"
+        case .deadCodeElimination: return "DCE"
+        case .differentiation: return "AD"
         case .linearAlgebraFusion: return "LAF"
+        case .literalBroadcastingPromotion: return "LBP"
+        case .matrixMultiplicationReordering: return "MMR"
         case .stackPromotion: return "SP"
         case .valuePromotion: return "VP"
-        case .matrixMultiplicationReordering: return "MMR"
-        case .literalBroadcastingPromotion: return "LBP"
         }
     }
 
     var description: String {
         switch self {
-        case .differentiation: return "differentiation"
-        case .deadCodeElimination: return "dead code elimination"
+        case .algebraSimplification: return "algebra simplification"
+        case .cfgCanonicalization: return "CFG canonicalization"
         case .commonSubexpressionElimination:
             return "common subexpression elimination"
-        case .algebraSimplification: return "algebra simplification"
+        case .deadCodeElimination: return "dead code elimination"
+        case .differentiation: return "differentiation"
         case .linearAlgebraFusion: return "linear algebra fusion"
-        case .stackPromotion: return "stack promotion"
-        case .valuePromotion: return "value promotion"
-        case .matrixMultiplicationReordering:
-            return "matrix multiplication reordering"
         case .literalBroadcastingPromotion:
             return "literal broadcasting promotion"
+        case .matrixMultiplicationReordering:
+            return "matrix multiplication reordering"
+        case .stackPromotion: return "stack promotion"
+        case .valuePromotion: return "value promotion"
         }
     }
 
@@ -69,33 +72,36 @@ public extension TransformPass {
     init?(rawValue: String) {
         typealias T = TransformPass
         switch rawValue {
-        case T.differentiation.abbrevation,
-             T.differentiation.rawValue:
-            self = .differentiation
-        case T.deadCodeElimination.abbrevation,
-             T.deadCodeElimination.rawValue:
-            self = .deadCodeElimination
-        case T.commonSubexpressionElimination.abbrevation,
-             T.commonSubexpressionElimination.rawValue:
-            self = .commonSubexpressionElimination
         case T.algebraSimplification.abbrevation,
              T.algebraSimplification.rawValue:
             self = .algebraSimplification
+        case T.cfgCanonicalization.abbrevation,
+             T.cfgCanonicalization.rawValue:
+            self = .cfgCanonicalization
+        case T.commonSubexpressionElimination.abbrevation,
+             T.commonSubexpressionElimination.rawValue:
+            self = .commonSubexpressionElimination
+        case T.deadCodeElimination.abbrevation,
+             T.deadCodeElimination.rawValue:
+            self = .deadCodeElimination
+        case T.differentiation.abbrevation,
+             T.differentiation.rawValue:
+            self = .differentiation
         case T.linearAlgebraFusion.abbrevation,
              T.linearAlgebraFusion.rawValue:
             self = .linearAlgebraFusion
+        case T.literalBroadcastingPromotion.abbrevation,
+             T.literalBroadcastingPromotion.rawValue:
+            self = .literalBroadcastingPromotion
+        case T.matrixMultiplicationReordering.abbrevation,
+             T.matrixMultiplicationReordering.rawValue:
+            self = .matrixMultiplicationReordering
         case T.stackPromotion.abbrevation,
              T.stackPromotion.rawValue:
             self = .stackPromotion
         case T.valuePromotion.abbrevation,
              T.valuePromotion.rawValue:
             self = .valuePromotion
-        case T.matrixMultiplicationReordering.abbrevation,
-             T.matrixMultiplicationReordering.rawValue:
-            self = .matrixMultiplicationReordering
-        case T.literalBroadcastingPromotion.abbrevation,
-             T.literalBroadcastingPromotion.rawValue:
-            self = .literalBroadcastingPromotion
         default:
             return nil
         }
@@ -106,24 +112,26 @@ extension TransformPass : StringEnumArgument {
     public static var completion: ShellCompletion {
         // NOTE: Can be shortened with enum iteration
         return .values([
-            (differentiation.abbrevation,
-             differentiation.description),
-            (deadCodeElimination.abbrevation,
-             deadCodeElimination.description),
-            (commonSubexpressionElimination.abbrevation,
-             commonSubexpressionElimination.description),
             (algebraSimplification.abbrevation,
              algebraSimplification.description),
+            (cfgCanonicalization.abbrevation,
+             cfgCanonicalization.description),
+            (commonSubexpressionElimination.abbrevation,
+             commonSubexpressionElimination.description),
+            (deadCodeElimination.abbrevation,
+             deadCodeElimination.description),
+            (differentiation.abbrevation,
+             differentiation.description),
             (linearAlgebraFusion.abbrevation,
              linearAlgebraFusion.description),
+            (literalBroadcastingPromotion.abbrevation,
+             literalBroadcastingPromotion.description),
+            (matrixMultiplicationReordering.abbrevation,
+             matrixMultiplicationReordering.description),
             (stackPromotion.abbrevation,
              stackPromotion.description),
             (valuePromotion.abbrevation,
-             valuePromotion.description),
-            (matrixMultiplicationReordering.abbrevation,
-             matrixMultiplicationReordering.description),
-            (literalBroadcastingPromotion.abbrevation,
-             literalBroadcastingPromotion.description)
+             valuePromotion.description)
         ])
     }
 }
@@ -132,32 +140,35 @@ public func runPass(_ pass: TransformPass, on module: Module,
                     bypassingVerification noVerify: Bool = false) throws {
     var changed: Bool
     switch pass {
-    case .differentiation:
-        changed = module.applyTransform(Differentiation.self,
-                                        bypassingVerification: noVerify)
-    case .deadCodeElimination:
-        changed = module.mapTransform(DeadCodeElimination.self,
+    case .algebraSimplification:
+        changed = module.mapTransform(AlgebraSimplification.self,
+                                      bypassingVerification: noVerify)
+    case .cfgCanonicalization:
+        changed = module.mapTransform(CFGCanonicalization.self,
                                       bypassingVerification: noVerify)
     case .commonSubexpressionElimination:
         changed = module.mapTransform(CommonSubexpressionElimination.self,
                                       bypassingVerification: noVerify)
-    case .algebraSimplification:
-        changed = module.mapTransform(AlgebraSimplification.self,
+    case .deadCodeElimination:
+        changed = module.mapTransform(DeadCodeElimination.self,
                                       bypassingVerification: noVerify)
+    case .differentiation:
+        changed = module.applyTransform(Differentiation.self,
+                                        bypassingVerification: noVerify)
     case .linearAlgebraFusion:
         changed = module.mapTransform(LinearAlgebraFusion.self,
+                                      bypassingVerification: noVerify)
+    case .literalBroadcastingPromotion:
+        changed = module.mapTransform(LiteralBroadcastingPromotion.self,
+                                      bypassingVerification: noVerify)
+    case .matrixMultiplicationReordering:
+        changed = module.mapTransform(MatrixMultiplicationReordering.self,
                                       bypassingVerification: noVerify)
     case .stackPromotion:
         changed = module.mapTransform(StackPromotion.self,
                                       bypassingVerification: noVerify)
     case .valuePromotion:
         changed = module.mapTransform(ValuePromotion.self,
-                                      bypassingVerification: noVerify)
-    case .matrixMultiplicationReordering:
-        changed = module.mapTransform(MatrixMultiplicationReordering.self,
-                                      bypassingVerification: noVerify)
-    case .literalBroadcastingPromotion:
-        changed = module.mapTransform(LiteralBroadcastingPromotion.self,
                                       bypassingVerification: noVerify)
     }
     print("\(pass.abbrevation):", changed ? "changed" : "unchanged")
